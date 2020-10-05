03e90554b93b75f9508559ba252796b33db7fcc4
Daniel <danisan9490@gmail.com>
2020-10-05 16:07:11 +0100

    #2 Create useSelect Hooks

--- client/src/components/Formulario.js
+++ client/src/components/Formulario.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import styles from './Formulario.module.css';
+import useSelect from '../hooks/useSelect';
 
 const Formulario = () => {
+
+  const OPCIONES = [
+    { value: 'general', label: 'General' },
+    { value: 'business', label: 'Business' },
+    { value: 'entertainment', label: 'Entertainment' },
+    { value: 'health', label: 'Health' },
+    { value: 'science', label: 'Science' },
+    { value: 'sports', label: 'Sports' },
+    { value: 'technology', label: 'Technology' },
+  ]
+
+  const [categoria, SelectNoticias] = useSelect('general', OPCIONES);
+
   return (
     <div className={`${styles.buscador} row`}>
       <div className="col s12 m8 offset-m2">
         <form>
           <h2 className={styles.heading}>Find News by Category</h2>
-
+          <SelectNoticias />
           <div className="input-field col s12">
             <input
               type="submit"
               className={`${styles['btn-block']}  btn-large amber darken-2`}
-              value="Buscar"
+              value="Search"
             />
           </div>
         </form>

e0a892b4e4f4e0e060ad1e5a03e350a17d6cc61a
Daniel <danisan9490@gmail.com>
2020-10-05 16:30:18 +0100

    Updates

---
+++ precache-manifest.b41d553cfebbff4eb8fef5a87bddd010.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbaead7c507ea96251417774fa05b3f7",
+    "url": "/noticias_react/index.html"
+  },
+  {
+    "revision": "2d99aca8e81ca2116383",
+    "url": "/noticias_react/static/css/main.f55c9bd3.chunk.css"
+  },
+  {
+    "revision": "ab7f2e5277fd5d3d7660",
+    "url": "/noticias_react/static/js/2.d52e35e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/noticias_react/static/js/2.d52e35e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d99aca8e81ca2116383",
+    "url": "/noticias_react/static/js/main.c2e8f852.chunk.js"
+  },
+  {
+    "revision": "a80d2fb867b8130abf54",
+    "url": "/noticias_react/static/js/runtime-main.4654826f.js"
+  }
+]);
